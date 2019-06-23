130deb540e6ff6047ff3eeca28299bf572e48126
Sangeetha DH <sangeetha.dh@accenture.com>
2019-06-23 20:25:25 +0530

    Maven SpringBatch template

---
+++ src/main/java/com/adapter/springbatchxmladapter/SpringBatchXmladapterApplication.java
@@ -0,0 +1,13 @@
+package com.adapter.springbatchxmladapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchXmladapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchXmladapterApplication.class, args);
+	}
+
+}
