c0bac30bfea432e3bae6a493a872772b68c81ac1
Sangeetha DH <sangeetha.dh@accenture.com>
2019-06-24 15:33:06 +0530

    Successful Application created

--- src/main/java/com/adapter/springbatchxmladapter/SpringBatchXmladapterApplication.java
+++ src/main/java/com/adapter/springbatchxmladapter/SpringBatchXmladapterApplication.java
@@ -2,12 +2,13 @@ package com.adapter.springbatchxmladapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class SpringBatchXmladapterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchXmladapterApplication.class, args);
+		System.out.println("Running successfully>>>>>>>>>>");
 	}
-
 }
